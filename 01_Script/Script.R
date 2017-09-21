9eed8b31d1c78b596abdfbe01667b81f9643f8e1
Kevin Ostheimer <info@impulsleistung.de>
2017-09-21 02:57:16 +0200

    Folder Sync

--- 01_Script/Script.R
+++ 01_Script/Script.R
@@ -541,3 +541,457 @@ att5[, cols] <- sapply(att5[, cols],as.numeric)
 
 ###########################
 
+# Create a list n containing the values: 5, 10, and 20
+n<-list(5,10,20)
+
+# Call map() on n with rnorm() to simulate three samples
+map(.x = n,.f = rnorm)
+
+###########################
+
+
+
+# Passing two lists into a function with two parameters, this looks a little unsafe...
+# rnorm(n, mean = 0, sd = 1)
+
+# Initialize n
+n <- list(5, 10, 20)
+
+# Create a list mu containing the values: 1, 5, and 10
+mu<-list(1,5,10)
+
+# Edit to call map2() on n and mu with rnorm() to simulate three samples
+map2(.x = n,.y = mu,.f = rnorm)
+
+###########################
+
+# Putting more than two arguments into a function requires pmap()
+# The list must match the order of the target function
+# Initialize n and mu
+n <- list(5, 10, 20)
+mu <- list(1, 5, 10)
+
+# Create a sd list with the values: 0.1, 1 and 0.1
+sd <- list(0.1,1,0.1)
+
+# Edit this call to pmap() to iterate over the sd list as well
+pmap(list(n, mu, sd), rnorm)
+
+###########################
+
+# That's the safe variant of using pmap. Don't use "" for the function arguments
+# Name the elements of the argument list
+pmap(list(mean = mu, n = n, sd = sd), rnorm)
+
+###########################
+
+# Calling a list of functions with a list of parameters in one line
+# Define list of functions
+f <- list("rnorm", "runif", "rexp")
+
+# Parameter list for rnorm()
+rnorm_params <- list(mean = 10)
+
+# Add a min element with value 0 and max element with value 5
+runif_params <- list(min = 0, max = 5)
+
+# Add a rate element with value 5
+rexp_params <- list(rate = 5)
+
+# Define params for each function
+params <- list(
+  rnorm_params,
+  runif_params,
+  rexp_params
+)
+
+# Call invoke_map() on f supplying params as the second argument
+# Since every function has the "n" parameter, it is defined in a common way
+invoke_map(.f = f, .x = params, n = 5)
+
+###########################
+
+# walk() operates just like map() except it's designed for functions that don't return anything.
+# You use walk() for functions with side effects like printing, plotting or saving.
+# Define list of functions
+f <- list(Normal = "rnorm", Uniform = "runif", Exp = "rexp")
+
+# Define params
+params <- list(
+  Normal = list(mean = 10),
+  Uniform = list(min = 0, max = 5),
+  Exp = list(rate = 5)
+)
+
+# Assign the simulated samples to sims
+sims<-invoke_map(f, params, n = 50)
+
+# Use walk() to make a histogram of each element in sims
+walk(sims, hist)
+
+str(sims)
+# List of 3
+#  $ Normal : num [1:50] 9.14 11.15 12.2 11.65 9.19 ...
+#  $ Uniform: num [1:50] 2.39 1.02 1.29 3.99 2.03 ...
+#  $ Exp    : num [1:50] 0.203 0.805 0.771 0.103 0.27 ...
+
+###########################
+
+# Safe iteration, using a for-loop to iterate over colums
+# Replace the 1:ncol(df) sequence
+for (i in seq_along(df)) {
+  print(median(df[[i]]))
+}
+
+# Change the value of df
+df<- data.frame(NULL)
+
+# Repeat for loop to verify there is no error
+# Replace the 1:ncol(df) sequence
+for (i in seq_along(df)) {
+  print(median(df[[i]]))
+}
+
+##########
+
+# Fast and save iteration by memory pre-allocation
+# [[ - Brackets for generalisation
+# Create new double vector: output
+output<-vector(mode = "double", length = ncol(df))
+
+# Alter the loop
+for (i in seq_along(df)) {
+  # Change code to store result in output
+  output[[i]]<-median(df[[i]])
+}
+
+# Print output
+output
+
+#########
+
+# Normalize a vector using range()
+# Define example vector x
+x <- 1:10
+
+# Define rng
+rng<-range(x,na.rm = TRUE)
+
+# Rewrite this snippet to refer to the elements of rng
+(x - rng[1]) /  (rng[2] - rng[1])
+
+############
+
+# Calculating the confidence interval and catch errors
+# Alter the mean_ci function
+mean_ci <- function(x, level = 0.95) {
+  if(length(x)==0) {
+    warning("'x' was empty",call. = FALSE)
+    return(c(-Inf,Inf)  )
+  }
+  se <- sd(x) / sqrt(length(x))
+  alpha <- 1 - level
+  mean(x) + se * qnorm(c(alpha / 2, 1 - alpha / 2))
+}
+
+#############
+
+# Create col_mean() function to find column means
+col_mean <- function(df) {
+  output <- numeric(length(df))
+  for (i in seq_along(df)) {
+    output[[i]] <- mean(df[[i]])
+  }
+  output
+}
+
+#############
+
+# All the map functions in purrr take a vector, .x, as the first argument, then return .f applied to each element of # .x. The type of object that is returned is determined by function suffix (the part after _):
+# map() returns a list or data frame
+# map_lgl() returns a logical vector
+# map_int() returns a integer vector
+# map_dbl() returns a double vector
+# map_chr() returns a character vector
+
+# Load the purrr package
+library(purrr)
+
+# Use map_dbl() to find column means
+map_dbl(.x = df, .f = mean)
+
+# Use map_dbl() to column medians
+map_dbl(.x = df, .f = median)
+
+# Use map_dbl() to find column standard deviations
+map_dbl(.x = df, .f = sd)
+
+#############
+
+# Find the mean of each column
+map_dbl(planes,mean)
+
+# Find the mean of each column, excluding missing values
+map_dbl(.x = planes, .f = mean, na.rm=TRUE)
+
+# Find the 5th percentile of each column, excluding missing values
+map_dbl(.x = planes, .f = quantile,  probs = 0.05, na.rm=TRUE)
+
+#############
+
+# Functions on columns by using the map() - function
+# Find the columns that are numeric
+map_lgl(df3,is.numeric)
+
+# Find the type of each column
+map_chr(df3, typeof)
+
+# Find a summary of each column
+map(df3,summary)
+
+#############
+
+# Anonymous function for linear regression
+# Rewrite to call an anonymous function
+map(cyl, function(df) lm(mpg ~ wt, data = df))
+
+## The short form by purrr - package is:
+map(cyl, ~ lm(.$mpg ~ .$wt))
+
+## Example
+# One way to do this would be to use an anonymous function:
+map_dbl(cyl, function(df) mean(df$disp))
+
+# To perform the same operation using the formula shortcut, we replace the function definition (function(df)) with the ~, then when we need to refer to the element of cyl the function operates on (in this case df), we use a ..
+
+map_dbl(cyl, ~ mean(.$disp))
+
+#############
+
+# Extract the second element from each vector in coefs using the numeric shortcut and map_dbl()
+coefs <- map(models, coef)
+
+# use map_dbl with the numeric shortcut to pull out the second element
+map_dbl(coefs,2)
+
+#############
+
+# Pipes in R %>%, the data input for the next function is automatically connected
+# Take a look at our code to get our list of models:
+
+cyl <- split(mtcars, mtcars$cyl) 
+map(cyl, ~ lm(mpg ~ wt, data = .))
+
+# We split the data frame mtcars and save it as the variable cyl. We then pass cyl as the first argument to map to fit the models. We could rewrite this using the pipe operator as:
+
+split(mtcars, mtcars$cyl) %>% 
+  map(~ lm(mpg ~ wt, data = .))
+
+# Define models (don't change)
+models <- mtcars %>% 
+  split(mtcars$cyl) %>%
+  map(~ lm(mpg ~ wt, data = .))
+
+# Rewrite to be a single command using pipes 
+# Single variables can also be piped
+models %>%
+  map(summary) %>%
+  map_dbl("r.squared")
+  
+#############
+
+# Try...catch by using safe() override
+# Create safe_readLines() by passing readLines() to safely()
+safe_readLines<-safely(readLines)
+
+# Call safe_readLines() on "http://example.org"
+safe_readLines("http://example.org")
+
+# Call safe_readLines() on 
+safe_readLines("http://asdfasdasdkfjlda")
+
+# Define safe_readLines()
+safe_readLines <- safely(readLines)
+
+# Use the safe_readLines() function with map(): html
+html<-map(urls,safe_readLines)
+
+# Call str() on html
+str(html)
+
+# Extract the result from one of the successful elements
+html[[1]]
+
+# Extract the error from the element that was unsuccessful
+html[[3]]
+
+#############
+
+# Inverting lists of pair into one pair of lists by transpose()
+
+str(html)
+# List of 3
+ # $ example:List of 2
+  # ..$ result: chr [1:50] "<!doctype html>" "<html>" "<head>" "    <title>Example Domain</title>" ...
+  # ..$ error : NULL
+ # $ rproj  :List of 2
+  # ..$ result: chr [1:121] "<!DOCTYPE html>" "<html lang=\"en\">" "  <head>" "    <meta charset=\"utf-8\">" ...
+  # ..$ error : NULL
+ # $ asdf   :List of 2
+  # ..$ result: NULL
+  # ..$ error :List of 2
+  # .. ..$ message: chr "cannot open the connection to 'http://asdfasdasdkfjlda'"
+  # .. ..$ call   : language file(con, "r")
+  # .. ..- attr(*, "class")= chr [1:3] "simpleError" "error" "condition"
+
+str(transpose(html))
+# List of 2
+ # $ result:List of 3
+  # ..$ example: chr [1:50] "<!doctype html>" "<html>" "<head>" "    <title>Example Domain</title>" ...
+  # ..$ rproj  : chr [1:121] "<!DOCTYPE html>" "<html lang=\"en\">" "  <head>" "    <meta charset=\"utf-8\">" ...
+  # ..$ asdf   : NULL
+ # $ error :List of 3
+  # ..$ example: NULL
+  # ..$ rproj  : NULL
+  # ..$ asdf   :List of 2
+  # .. ..$ message: chr "cannot open the connection to 'http://asdfasdasdkfjlda'"
+  # .. ..$ call   : language file(con, "r")
+  # .. ..- attr(*, "class")= chr [1:3] "simpleError" "error" "condition"
+  
+# Define save_readLines() and html
+safe_readLines <- safely(readLines)
+html <- map(urls, safe_readLines)
+
+# The safely() function generates a list with "result" and "error" for each request on each element that was requested
+# Examine the structure of transpose(html)
+str(transpose(html))
+
+# Extract the results: res
+res  <-transpose(html)[["result"]]
+
+# Extract the errors: errs
+errs <-transpose(html)[["error"]]
+
+#############
+
+# These two should be used on the result of safely()
+res <- transpose(XXXX)[["result"]]
+errs <- transpose(XXXX)[["error"]]
+
+#############
+
+# Handle results of safely()
+# Initialize some objects
+safe_readLines <- safely(readLines)
+html <- map(urls, safe_readLines)
+res <- transpose(html)[["result"]]
+errs <- transpose(html)[["error"]]
+
+# Create a logical vector is_ok
+is_ok<-map_lgl(errs, is.null)
+
+# Extract the successful results
+res[is_ok]
+
+# Extract the input from the unsuccessful results
+urls[!is_ok]
+
+#############
+
+# Using walk2 - Walk is an alternative to map that you use when you want to call a function for its side effects
+# Replace "Sturges" with reasonable breaks for each sample
+breaks_list <- list(
+  Normal = seq(6, 16, 0.5),
+  Uniform = seq(0, 5, 0.25),
+  Exp = seq(0, 1.5, 0.1)
+)
+
+# Use walk2() to make histograms with the right breaks
+# Definition: walk2(.x, .y, .f, ...)
+# Definition: hist(x, ...) # Default S3 method: hist(x, breaks = "Sturges", ... )
+walk2(.x = sims, .y = breaks_list, .f = hist)
+
+
+#############
+
+# Increase sample size to 1000
+sims <- invoke_map(f, params, n = 1000)
+
+# Compute nice_breaks (don't change this)
+nice_breaks <- map(sims, find_breaks)
+
+# Create a vector nice_titles
+nice_titles<-c("Normal(10, 1)", "Uniform(0, 5)", "Exp(5)")
+
+# Use pwalk() instead of walk2()
+pwalk(.l =  list(x=sims, breaks=nice_breaks,main=nice_titles), .f = hist, xlab="")
+
+str(f)
+# List of 3
+ # $ Normal : chr "rnorm"
+ # $ Uniform: chr "runif"
+ # $ Exp    : chr "rexp"
+ 
+> str(params)
+# List of 3
+ # $ Normal :List of 1
+  # ..$ mean: num 10
+ # $ Uniform:List of 2
+  # ..$ min: num 0
+  # ..$ max: num 5
+ # $ Exp    :List of 1
+  # ..$ rate: num 5
+  
+#############
+
+# One of the nice things about the walk() functions is that they return the object you passed to them. This means they can easily be used in pipelines (a pipeline is just a short way of saying "a statement with lots of pipes").
+
+# Pipe this along to map(), using summary() as .f
+sims %>%
+  walk(hist) %>%
+  map(summary)
+  
+str(sims)
+# List of 3
+ # $ Normal : num [1:50] 9.17 9.39 11.01 9.79 9.07 ...
+ # $ Uniform: num [1:50] 4.28 1.741 4.32 0.361 3.628 ...
+ # $ Exp    : num [1:50] 0.11546 0.0546 0.00364 0.43626 0.11729 ...
+
+#############
+ 
+# HowTo halt on unplausible inputs
+# Define troublesome x and y
+x <- c(NA, NA, NA)
+y <- c( 1, NA, NA, NA)
+
+both_na <- function(x, y) {
+  # Add stopifnot() to check length of x and y
+  stopifnot(length(x)==length(y))
+  
+  sum(is.na(x) & is.na(y))
+}
+
+# Call both_na() on x and y
+both_na(x, y)
+
+#############
+
+# HowTo throw an error in the correct way
+# Define troublesome x and y
+x <- c(NA, NA, NA)
+y <- c( 1, NA, NA, NA)
+
+both_na <- function(x, y) {
+  # Replace condition with logical
+  if (length(x)!=length(y)) {
+    # Replace "Error" with better message
+    stop("x and y must have the same length", call. = FALSE)
+  }  
+  
+  sum(is.na(x) & is.na(y))
+}
+
+# Call both_na() 
+both_na(x, y)
+
+#############
